219cc6746de5644c055269b8963b5e8f27ca70ac
Yashaswini1308 <yashaswiniputta09@gmail.com>
2024-03-18 22:56:47 -0500

    Added new files and modified app.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET this page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('mydata', { title: 'Sai Shivani Yashaswini Putta' });
 });
 
 module.exports = router;
