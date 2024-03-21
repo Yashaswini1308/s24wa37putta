6b0735351c2b9f57bde8a1a2fc3e7caabd58985a
Yashaswini1308 <yashaswiniputta09@gmail.com>
2024-03-20 20:21:07 -0500

    BONUS

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
+var computationRouter = require('./routes/computation');
+
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
+app.use('/computation', computationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/computation.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+/* GET users listing. */
+router.get('/', (req, res) => {
+    const x = req.query.x || Math.round(Math.random() * 99);
+    const cosVal = Math.cos(x);
+    const asinVal = Math.asin(x);
+    const asinhVal = Math.asinh(x);
+
+    const response = `
+        Math.cos() applied to ${x} is ${cosVal}<br>
+        Math.asin() applied to ${x} is ${asinVal}<br>
+        Math.asinh() applied to ${x} is ${asinhVal}
+    `;
+
+    res.send(response);
+});
+
+module.exports = router;
